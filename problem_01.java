ef46efd0546d5b642d01e34159667bf68ceb02e6
kuldeep maurya <kuldeepmaurya4296@gmail.com>
2023-07-05 00:30:37 +0530

    Add files via upload

---
+++ problem_01.java
@@ -0,0 +1,9 @@
+public class problem_01 {
+    // Sum of two integers
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 7;
+        int c = a+b;
+        System.out.println(c);
+    }
+}
