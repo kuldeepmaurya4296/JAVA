a1ac44628af0c4397629022859c1ccb0bae4c6ed
kuldeep maurya <kuldeepmaurya4296@gmail.com>
2023-07-05 00:31:24 +0530

    Add files via upload

---
+++ problem_02.java
@@ -0,0 +1,11 @@
+public class problem_02 {
+    public static void main(String[] args) {
+        //Print the messege along with values and sum
+        //	Ex - The sum of 45 & 12  = 57
+
+        int a = 2;
+        int b = 7;
+        int c = a+b;
+        System.out.println("The sum of "+a+" & "+b+"  = "+c);
+    }
+}
